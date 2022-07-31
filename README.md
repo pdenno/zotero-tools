26e13e042f811c52791544e43776e48b4e533290
Peter Denno <podenno@gmail.com>
2022-07-31 12:59:13 -0400

    Datascript example

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 # zotero-tools
-An example to explore Clojurescript, shadow-cljs, and zotero simultaneously.
+An example to explore [Clojurescript](https://clojurescript.org/),
+[shadow-cljs](https://shadow-cljs.github.io/docs/UsersGuide.html),
+[Datascript](https://github.com/tonsky/datascript),
+[Zotero](https://www.zotero.org/) and maybe
+[FulcroRAD](https://book.fulcrologic.com/) simultaneously.
 
-See the zotero-tools.org file for more information.
+# Installing
+
+ Install shadow-cljs globally:
+  `sudo npm install -g shadow-cljs`
+
+ In the project directory:
+  1. `npm init`
+  2. `npm install zotero-api-client`
+  3. `npm install shadow-cljs`
+
+# Building/running
+
+[comment]: # ## The script
+[comment]: #   The current script does nothing much, but the plan is to generate EDN or JSON corresponding to a Zotero Library.
+[comment]: # 
+[comment]: # At a shell prompt:
+[comment]: # ```bash
+[comment]: #  shadow-cljs compile script                # This creates target/script.js (see ./shadow-cljs.edn, :target :script)
+[comment]: #  node --trace-warnings target/script.js    # Run the script. (--tracew-warnings is optional.)
+[comment]: # ```
+
+## The server (in emacs, for development)
+
+The "server" is currently just a minimal http server process that continues to run so that you can hot-load code from the REPL.
+I use emacs/cider. Thus I'm starting a REPL from a shell and connecting to it through NREPL.
+
+At a shell prompt in the project directory run the following to compile and start the NREPL process:
+```bash
+  shadow-cljs -d nrepl/nrepl:0.9.0 -d cider/piggieback:0.5.3 -d cider/cider-nrepl:0.28.5 watch server
+```
+
+The response should include *shadow-cljs - nREPL server started on port <some-port-number>* which is the port
+you will use to connect the CIDER REPL to running application.
+But first, in another shell, you need to start the server.
+So in the second shell, in the project directy, run `node ./target/server.js.
+The program should respond with:
+
+```bash
+starting server
+http server running
+shadow-cljs - #3 ready!
+```
+
+If you do the above, then when you try to interact with code in a CIDER REPL, it will report **No available JS runtime.**
+If at anytime you see that warning, you can restart the server with `node target/script.js`.
+
+Now you can connect with CIDER using `M-x cider-connect-cljs` and the port number observer earlier.
+Respond with *shadow* as the REPL type and *:server* as the target compilation.
+
+Note also that shadow-clsj implements a handy tool for inspection and compilation at http://localhost:9630/.
--- src/server/zotero_tools/server.cljs
+++ src/server/zotero_tools/server.cljs
@@ -1,6 +1,7 @@
 (ns zotero-tools.server
   (:require
    [taoensso.timbre :as log]
+   [datascript.core :as d]
    ["http" :as http]
    ["zotero-api-client$default" :as zotero])) ; https://shadow-cljs.github.io/docs/UsersGuide.html#js-deps
 
@@ -10,7 +11,9 @@
 ;;; A place to hang onto the server so we can stop/start it.
 (defonce server-ref (volatile! nil))
 (defonce zserver-ref (volatile! nil))
-(defonce private-key (atom nil))
+;;; A Zotero library private key used for connection:
+(defonce private-key  (atom "P6mNyFcD6jebZmpz3tbxmKBx")) ; <========================
+(def user-id (atom 3000340))
 
 (defn main [& _args]
   (js/console.log "starting server")
@@ -52,29 +55,49 @@
 
 ;;;   const response = await api().library('user', 475425).collections('9KH9TNSJ').items().get();
 (defn get-data []
-  (log/info "Zotero client query")
-  (try (-> (zotero. "<private key here>") ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<================
-           (.library "user" 3000340)
-           ;; You don't have to focus on a particular collection; the .collections step can be commented out.
-           ;; Currenlty, the only way I know of learning collect keys is to run without this and look at :collections key.
-           (.collections "9N7626KR")
-           .items
-           .get
-           (.then #(reset! response  %))
-           (.catch #(js/console.log %))
-           (.finally #(if (-> response deref empty?)
-                        (js/console.log "Did you forget to specify the library private key?")
-                        (js/console.log "cleanup"))))
+  (log/info "get-data")
+  (let [server (if @private-key
+                 (zotero. @private-key)
+                 (throw (ex-info "You need to specify the library private key." {:key @private-key})))]
+    (vreset! zserver-ref server)
+    (try (-> server
+             (.library "user" @user-id)
+             ;; You don't have to focus on a particular collection; the .collections step can be commented out.
+             ;; Currenlty, the only way I know of learning collection keys is to run without this and look at :collections key.
+             (.collections "9N7626KR")
+             .items
+             .get ; https://clojurescript.org/guides/promise-interop (this is "promise resolve"?)
+             (.then #(reset! response  %))
+             (.catch #(js/console.log %))
+             (.finally #(js/console.log "cleanup")))
        (catch :default e
-         (ex-info "get-data didn't go well." {:e e}))))
+         (ex-info "get-data didn't go well." {:e e})))))
 
 ;;; const items = response.getData();
 ;;; console.log(items.map(i => i.title));
 (defn show-data []
-  (as-> @response ?d
-    (.getData ?d)
-    (js->clj ?d :keywordize-keys true)
-    (doall (map #(let [title (:title %)]
-                   (when (not= title "PDF")
-                     (log/info "Title:" title)))
-                ?d))))
+  (log/info "show-data")
+  (try 
+    (as-> @response ?d
+      (.getData ?d)
+      (js->clj ?d :keywordize-keys true)
+      (doall (map #(let [title (:title %)]
+                     (when (not= title "PDF")
+                       (log/info "Title:" title)))
+                  ?d)))
+    (catch :default e
+      (ex-info "show-data didn't go well." {:e e}))))
+
+;;; It doesn't like the datahike vector of maps with :db/ident.
+;(def schema [{:db/ident :person/aka :db/cardinality :db.cardinality/many}])
+(def schema {:person/name {:db/cardinality :db.cardinality/one  :db/valueType :db.type/string}
+             :person/age  {:db/cardinality :db.cardinality/one  :db/valueType :db.type/long}
+             :person/aka  {:db/cardinality :db.cardinality/many :db/valueType :db.type/string}})
+
+(def conn (d/create-conn schema))
+
+;; Implicit join, multi-valued attribute
+(defn ds-tryme []
+    (d/transact! conn [ {:person/name  "Maksim"
+                         :person/age   45
+                         :person/aka   ["Maks Otto von Stirlitz", "Jack Ryan"] } ]))
